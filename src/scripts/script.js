0681b316f17584589fdd70e2635984c35b60ae6d
Rodrigo Silva <rodrigo.codeshub25@gmail.com>
2025-05-19 21:09:52 -0300

    inicial do .html e do .js(2)

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,7 +5,7 @@ submitVar.addEventListener("click", function(){
     let passwordVar = document.getElementById("inputPassword").value
 
     if (emailVar == "rodrigo@email.com" && passwordVar == "123") {
-      window.location.href="dashboard.html"
+      window.location.replace="dashboard.html"
     }
 
 })
